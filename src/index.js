35ab0aed13194289bcc2a9adbf3b4adab7b162bd
Lucas Tanaka <lkt@pinhaotec.com.br>
2022-09-19 12:47:42 -0300

    Fixing response status (?)

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
     }
     todoFound.title = title;
     todoFound.deadline = new Date(deadline);
-    return response.status(204).send();
+    return response.status(200).send();
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
@@ -81,14 +81,14 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
     return response.status(404).json({error:"Todo not found"}).send();
   }
   todoFound.done = true;
-  return response.status(204).send();
+  return response.status(200).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const {user} = request;
   const {id} = request.params;
   const todoFound = user.todos.find((todo) => todo.id == id);
-  
+
   if (!todoFound){
     return response.status(404).json({error:"Todo not found"}).send();
   }
